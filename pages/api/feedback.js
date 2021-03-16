00389f1e36e11f707cff49c09d6d70ea50a3057b
Cloudbop <boppincolin@gmail.com>
2021-03-16 22:11:15 +0000

    added server side feedback

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,8 +1,34 @@
+import path from "path";
+import fs from "fs";
 // server side code
 function handler(req, res) {
   // express-like syntax !express though
 
-  res.status(200).json({ message: "This works" });
+  if ((req.method = "POST")) {
+    // client request
+    const email = req.body.email;
+    const text = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text
+    };
+    // create absolute path to file
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    // synchronous read
+    const fileData = fs.readFileSync(filePath);
+    //
+    const data = JSON.parse(fileData);
+    // add datea
+    data.push(newFeedback);
+    // syncronous write
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    // send back result
+    res.status(201).json({ message: "Success!", feedback: newFeedback });
+  } else {
+    // res.status(200).json({ message: "This works" });
+  }
 }
 
 export default handler;
